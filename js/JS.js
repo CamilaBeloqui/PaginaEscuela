477cf3493982cc06b353fb82444e5e48b3ba8668
Camila Beloqui <camilabeloqui925@gmail.com>
2021-08-05 16:55:56 -0300

    Inicializando proyecto2

---
+++ js/JS.js
@@ -0,0 +1,36 @@
+alert ("Hola")
+var etiquetas = document.createElement("h1");
+var contenido = document.createTextNode("Caulher");
+var contenido2 = document.createTextNode("Servicios Integrales");
+etiquetas.appendChild.contenido;
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+//window.status
+
+
+//(function detectSO(){}
+//var osName = "Desconocido";
+//if(navigator.userAgent.indexOf("Win") !=1){
+  //  osName = "Windows";
+//}
+//if(navigator.userAgent.indexOf("Android") !=1){
+  //  osName = "Android";
+//}
+  //  if (osName == "Windows"){
+    //    window.location
+    //}
+    //else{  
+      //   alert("No tenemos una web");
+    //} 
